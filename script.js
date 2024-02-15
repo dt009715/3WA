04b7b98280011237c40c88d5ce6d2f687ee2ab2f
Chloé <chloedrieux67@gmail.com>
2024-02-15 12:11:22 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,4 +3,8 @@ fetch('https://cdn.taux.live/api/ecb.json')
     .then(data => {
         console.log(data)
         console.log(data.rates.EUR)
+        data.rates.forEach(rate => {
+            console.log(rate)
+        })
+            
     })
